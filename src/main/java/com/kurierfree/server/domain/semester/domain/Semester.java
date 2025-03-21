d458b56743c6f173c3a6d7d5d58bc22f4ac79a0c
yeonjy73 <yeon703@naver.com>
2025-03-22 03:13:23 +0900

    feat: add Semester Class

---
+++ src/main/java/com/kurierfree/server/domain/semester/domain/Semester.java
@@ -0,0 +1,21 @@
+package com.kurierfree.server.domain.semester.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "semester")
+public class Semester {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private SemesterTime semesterTime;
+}
--- src/main/java/com/kurierfree/server/domain/user/domain/Supporter.java
+++ src/main/java/com/kurierfree/server/domain/user/domain/Supporter.java
@@ -1,15 +1,18 @@
 package com.kurierfree.server.domain.user.domain;
 
+import com.kurierfree.server.domain.semester.domain.Semester;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Table(name = "supporter")
 public class Supporter extends User{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Semester semester;
 
     @Column(nullable = false)
     private ApplicationStatus status;
