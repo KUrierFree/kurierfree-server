69612ba7667671f9782ad540f75e092e50a0d859
yeonjy73 <yeon703@naver.com>
2025-03-22 13:17:41 +0900

    fix: 오타 수정

--- src/main/java/com/kurierfree/server/domain/user/dao/SupporterRepository.java
+++ src/main/java/com/kurierfree/server/domain/user/dao/SupporterRepository.java
@@ -3,5 +3,5 @@ package com.kurierfree.server.domain.user.dao;
 import com.kurierfree.server.domain.user.domain.Supporter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SupprterRepository extends JpaRepository<Supporter, Long> {
+public interface SupporterRepository extends JpaRepository<Supporter, Long> {
 }
