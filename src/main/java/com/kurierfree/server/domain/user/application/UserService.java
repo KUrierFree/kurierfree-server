072501e0308dd47624457b2628f6bf0c72c01946
yeonjy73 <yeon703@naver.com>
2025-03-23 17:11:14 +0900

    delete: user Repository delete

--- src/main/java/com/kurierfree/server/domain/user/application/UserService.java
+++ src/main/java/com/kurierfree/server/domain/user/application/UserService.java
@@ -1,10 +1,8 @@
 package com.kurierfree.server.domain.user.application;
 
 import com.kurierfree.server.domain.semester.application.SemesterService;
-import com.kurierfree.server.domain.semester.domain.Semester;
 import com.kurierfree.server.domain.user.dao.DisabledStudentRepository;
 import com.kurierfree.server.domain.user.dao.SupporterRepository;
-import com.kurierfree.server.domain.user.dao.UserRepository;
 import com.kurierfree.server.domain.user.domain.DisabledStudent;
 import com.kurierfree.server.domain.user.domain.Role;
 import com.kurierfree.server.domain.user.domain.Supporter;
--- src/main/java/com/kurierfree/server/domain/user/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kurierfree.server.domain.user.dao;
-
-import com.kurierfree.server.domain.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
