6e54d040ef9122eb76e934940e7f8a5c7250f262
Chae-Yu <chaeyu2004@naver.com>
2025-03-31 12:39:51 +0900

    fix: dto 오류 수정

---
+++ src/main/java/com/kurierfree/server/domain/list/dto/response/DisabledStudentResponse.java
@@ -0,0 +1,20 @@
+package com.kurierfree.server.domain.list.dto.response;
+
+
+import com.kurierfree.server.domain.user.domain.enums.DisabilityType;
+import com.kurierfree.server.domain.user.domain.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DisabledStudentResponse {
+    private Long disabledStudentId;
+    private String name;
+    private String department;
+    private Gender gender;
+    private DisabilityType disabilityType;
+    private boolean matchingStatus;
+}
