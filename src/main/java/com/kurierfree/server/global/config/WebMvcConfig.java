0e516feae89fea0a575f7515032ecd7d3e8d2736
yeonjy73 <yeon703@naver.com>
2025-04-06 23:14:11 +0900

    feat: 토큰을 통한 admin 검증 로직 추가

--- src/main/java/com/kurierfree/server/global/config/WebMvcConfig.java
+++ src/main/java/com/kurierfree/server/global/config/WebMvcConfig.java
@@ -1,12 +1,19 @@
 package com.kurierfree.server.global.config;
 
+import com.kurierfree.server.domain.auth.Interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
+    private AuthInterceptor authInterceptor;
+
+    public WebMvcConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,5 +23,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
     }
 }
