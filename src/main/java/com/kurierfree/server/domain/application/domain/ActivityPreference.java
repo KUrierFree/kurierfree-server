61d7bb8d1b380d013ba2b95f2ee0eec318548630
Chae-Yu <chaeyu2004@naver.com>
2025-05-28 02:45:04 +0900

    feat: 지원서 도메인 작성
    - 희망 우선순위 표 Embeddable 클래스로 작성
    - null 값 입력 가능

--- src/main/java/com/kurierfree/server/domain/application/domain/ActivityPreference.java
+++ src/main/java/com/kurierfree/server/domain/application/domain/ActivityPreference.java
@@ -1,8 +1,14 @@
 package com.kurierfree.server.domain.application.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
 public class ActivityPreference {
     private int priority;
     private String preferredActivity;
