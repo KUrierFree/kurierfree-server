923cc6939e51be20a858ddbec8a9ce2ee2308ef7
Chae-Yu <chaeyu2004@naver.com>
2025-05-28 02:42:32 +0900

    refactor: 지원서 Builder 추가

--- src/main/java/com/kurierfree/server/domain/application/domain/Application.java
+++ src/main/java/com/kurierfree/server/domain/application/domain/Application.java
@@ -1,12 +1,10 @@
 package com.kurierfree.server.domain.application.domain;
 
 import com.kurierfree.server.domain.application.domain.enums.ActivityType;
+import com.kurierfree.server.domain.application.dto.request.ApplicationRequest;
 import com.kurierfree.server.domain.user.domain.enums.Gender;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,4 +60,38 @@ public class Application {
     @CollectionTable(name = "activity_preferences", joinColumns = @JoinColumn(name = "application_id"))
     @OrderBy("priority ASC")
     private List<ActivityPreference> activityPreference = new ArrayList<>();
+
+    @Builder
+    public Application(String department, int studentId, String grade, String name,
+                       Gender gender, String birthDate, Boolean hasExperience, String email,
+                       String phoneNum, ActivityType activityType, String volunteerExperience) {
+        this.department = department;
+        this.studentId = studentId;
+        this.grade = grade;
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.hasExperience = hasExperience;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.activityType = activityType;
+        this.volunteerExperience = volunteerExperience;
+        this.activityPreference = new ArrayList<>();
+    }
+
+    public static Application of(ApplicationRequest request) {
+        return Application.builder()
+                .department(request.getDepartment())
+                .studentId(request.getStudentId())
+                .grade(request.getGrade())
+                .name(request.getName())
+                .gender(request.getGender())
+                .birthDate(request.getBirthDate())
+                .hasExperience(request.getHasExperience())
+                .email(request.getEmail())
+                .phoneNum(request.getPhoneNum())
+                .activityType(request.getActivityType())
+                .volunteerExperience(request.getVolunteerExperience())
+                .build();
+    }
 }
